1f984c91c57c58dec38a1926490f10ee66306d71
Nik Yerasi <nyerasi@yahoo.com>
2020-07-05 21:00:01 -0700

    added rest UI

---
+++ ParkinsonsDataCollection/View Controllers/RestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RestViewController.swift
+//  ParkinsonsDataCollection
+//
+//  Created by Nikhil Yerasi on 7/5/20.
+//  Copyright © 2020 nyerasi. All rights reserved.
+//
+
+import UIKit
+
+class RestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
